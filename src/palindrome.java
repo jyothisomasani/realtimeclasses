6637697962088616ea163d85ae95eeb6c6186834
jyothisomasani <jyothisomasani@gmail.com>
2023-02-02 22:01:29 -0600

    2/2/2023-string programs

--- src/palindrome.java
+++ src/palindrome.java
@@ -5,10 +5,18 @@ public class palindrome {
         String str;
         Scanner sc = new Scanner(System.in);
         str = sc.nextLine();
+        String str1="";
+
         for(int i=str.length()-1;i>=0;i--)
         {
             System.out.print(str.charAt(i));
+            str1=str1+str.charAt(i);
         }
+        if(str.equals(str1))
+            System.out.println("palindrome");
+        else{
+            System.out.println("\n");
+            System.out.println("not palindrome");}
 
     }
 }
